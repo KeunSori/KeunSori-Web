e1a345d88cb084a46d669a1dbc8253bd39abf42a
Emithen <86219540+Emithen@users.noreply.github.com>
2025-02-20 16:03:44 +0900

    refactor: 회원가입 컴포넌트
    스타일 코드 정리

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,49 +1,49 @@
-import { css } from "@emotion/css";
 import logo from "/image/logo.svg";
 import { Link } from "react-router-dom";
 import SignUpForm from "../components/SignUp/SignUpForm.tsx";
+import styled from "@emotion/styled";
 
 const SignUpPage = () => {
   return (
     <>
-      <div
-        className={css`
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-direction: column;
-          gap: 1px;
-          height: 100vh;
-          min-height: 100vh;
-          margin-top: -5vh;
-        `}
-      >
+      <Ground>
         <Link to="/login">
-          <img
-            className={css`
-              margin-top: 60px;
-              margin-bottom: 10px;
-              width: 210px;
-              max-width: 90%;
-              @media (max-width: 768px) {
-                width: 150px;
-                margin-top: 40px;
-              }
-            `}
-            src={logo}
-            alt="logo"
-          />
+          <Logo src={logo} alt="logo" />
         </Link>
+
+        {/* 회원 가입 폼 */}
         <SignUpForm />
-        <div
-          className={css`
-            color: rgb(98, 98, 98);
-          `}
-        >
-          * 가입 승인이 될 때까지 며칠 시간이 소요될 수 있습니다.
-        </div>
-      </div>
+
+        <Notice>* 가입 승인이 될 때까지 며칠 시간이 소요될 수 있습니다.</Notice>
+      </Ground>
     </>
   );
 };
+
 export default SignUpPage;
+
+const Ground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1px;
+  height: 100vh;
+  min-height: 100vh;
+  margin-top: -5vh;
+`;
+
+const Logo = styled.img`
+  margin-top: 60px;
+  margin-bottom: 10px;
+  width: 210px;
+  max-width: 90%;
+  @media (max-width: 768px) {
+    width: 150px;
+    margin-top: 40px;
+  }
+`;
+
+const Notice = styled.div`
+  color: rgb(98, 98, 98);
+`;
