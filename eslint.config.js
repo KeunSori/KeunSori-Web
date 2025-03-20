f093ceae349c563949ad7bffe8b82346365458b3
leejuhi <anim0203@naver.com>
2025-03-20 14:53:35 +0900

    chore: any 타입 제거

--- eslint.config.js
+++ eslint.config.js
@@ -19,7 +19,6 @@ export default [
     rules: {
       "react/react-in-jsx-scope": "off", // React 17 이상에서는 불필요
       "react/jsx-no-target-blank": ["error", { allowReferrer: true }], // 보안 문제 해결
-      "@typescript-eslint/no-explicit-any": "off",
       "react/prop-types": "off",
     },
   },
