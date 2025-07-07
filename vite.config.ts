96ba70b18413ccced49e5ae8bd79270fc91af17a
Lee JuHee <163210518+BeanMouse@users.noreply.github.com>
2025-07-07 22:48:03 +0900

    Fix: config.ts 파일 수정 (#171)
    * fix: 안 쓰는 토큰 버리고 배포 오류 수정
    
    * chore: 토큰 관련 코드 삭제
    
    * chore: 회원가입 주석처리
    
    * chore: 로그아웃 주석 처리
    
    * chore: update configfile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import fs from "fs";
+// import fs from "fs";
+
 
 export default defineConfig({
   plugins: [react()],
@@ -10,11 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  server: {
-    port: 5173,
-    https: {
-      key: fs.readFileSync("./ssl/key.pem"),
-      cert: fs.readFileSync("./ssl/cert.pem"),
-    },
-  },
+  // server: {
+  //   port: 5173,
+  //   https: {
+  //     key: fs.readFileSync("./ssl/key.pem"),
+  //     cert: fs.readFileSync("./ssl/cert.pem"),
+  //   },
+  // },
 });
